cbb466225c0687c596fc8dc1dc8635564adaa8a3
Steve Coursen <smcoursen@gmail.com>
2013-04-30 16:24:10 -0400

    Fix sort bug, support 'summary' field

--- simple/models.py
+++ simple/models.py
@@ -1,12 +1,15 @@
+from functools import partial
 from BeautifulSoup import BeautifulSoup
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.core.urlresolvers import reverse
-
+from django.core import cache
+from django.utils.hashcompat import md5_constructor
 # Create your models here.
 from datetime import datetime
 import time
 from pyes import ES
-from pyes.query import MatchAllQuery, FilteredQuery
+from pyes.query import MatchAllQuery, FilteredQuery, TermQuery
 from pyes.filters import TermFilter, TermsFilter, ANDFilter
 from django.conf import settings
 
@@ -49,17 +52,17 @@ def get_comments(*order_by, **kwargs):
         q = FilteredQuery(MatchAllQuery(), ANDFilter(filters))
     else:
         q = MatchAllQuery()
-    ordering = {}
+    ordering = []
     for field in order_by:
         if field.startswith('-'):
-            field = field[1:]
-            ordering[field] = 'desc'
+            ordering.append(':'.join([field[1:], 'desc']))
         else:
-            ordering[field] = 'asc'
-    if ordering:
-        q = q.search(sort=ordering)
+            ordering.append(field)
     es = get_connection()
-    rs = es.search(q, ELASTICSEARCH_INDEX, 'comment')
+    sfunc = partial(es.search, q, ELASTICSEARCH_INDEX, 'comment')
+    if ordering:
+        sfunc = partial(sfunc, sort=ordering[0])
+    rs = sfunc()
     results = []
     for row in rs:
         row['reference'] = get_reference(row['reference_to'], row['reference_type'])
@@ -69,6 +72,23 @@ def get_comments(*order_by, **kwargs):
 
 def get_comment_count(**kwargs):
     return len(get_comments(**kwargs))
+
+def get_post_by_slug(slug):
+    es = get_connection()
+    posts = es.search(FilteredQuery(MatchAllQuery(), TermFilter('slug', slug)), ELASTICSEARCH_INDEX, 'post')
+    if not posts.total:
+        raise ObjectDoesNotExist
+    obj = posts[0]
+    obj['author'] = get_author(id=obj['author'])
+    if 'created_on' in obj:
+        obj['created_on'] = _parse_datetime(obj['created_on'])
+    else:
+        obj['created_on'] = datetime.utcnow()
+    if 'updated_on' in obj:
+        obj['updated_on'] = _parse_datetime(obj['updated_on'])
+    else:
+        obj['updated_on'] = datetime.utcnow()
+    return obj
 
 def get_post(author, blog_id):
     es = get_connection()
@@ -87,19 +107,17 @@ def get_post(author, blog_id):
 def get_posts(*order_by):
     es = get_connection()
     query = MatchAllQuery()
-    ordering = {}
+    ordering = []
     for field in order_by:
         if field.startswith('-'):
-            field = field[1:]
-            ordering[field] = 'desc'
+            ordering.append(':'.join([field[1:], 'desc']))
         else:
-            ordering[field] = 'asc'
+            ordering.append(field)
+    sfunc = partial(es.search, query, ELASTICSEARCH_INDEX, 'post')
     if ordering:
-        query = query.search(sort=ordering)
-    else:
-        query = query.search()
+        sfunc = partial(sfunc, sort=ordering[0])
+    rs = sfunc()
     author_cache = {}
-    rs = es.search(query, ELASTICSEARCH_INDEX, 'post', scan=True)
     results = [row for row in rs]
     for row in results:
         try:
@@ -120,14 +138,28 @@ def get_posts(*order_by):
             row['updated_on'] = datetime.utcnow()
     return results
 
-def index_post(author, post):
+def index_post(author, post, slug=None):
     es = get_connection()
     author_id = author.get_meta().id
+    if slug:
+        post_id = slug
+    if post.get('slug'):
+        post_id = post['slug']
+    else:
+        try:
+            post_id = post.get_meta().id
+        except AttributeError:
+            post_id = None
     post['author'] = author_id
-    return es.index(post, ELASTICSEARCH_INDEX, 'post', parent=author_id)._id
+    return es.index(post, ELASTICSEARCH_INDEX, 'post', id=post_id, parent=author_id)._id
 
 def index_comment(comment):
     es = get_connection()
+    post_ref = comment.get('post')
+    if post_ref:
+        var_key = md5_constructor(post_ref).hexdigest()
+        all_key = 'template.cache.comments.%s' % var_key
+        cache.get_cache('default').delete(all_key)
     return es.index(comment, ELASTICSEARCH_INDEX, 'comment')._id
 
 def create_author(**kwargs):
