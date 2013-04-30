557dabef00901e5a275ac3ecbd44a44e66894399
Steve Coursen <smcoursen@gmail.com>
2013-04-30 16:25:15 -0400

    New fields for mapping

--- simple/management/commands/put_mappings.py
+++ simple/management/commands/put_mappings.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 import pyes
 from pyes import mappings
+ELASTICSEARCH_URL = getattr(settings, 'ELASTICSEARCH_URL', 'http://localhost:9200/')
+ELASTICSEARCH_INDEX = getattr(settings, 'ELASTICSEARCH_INDEX', 'blog')
 
 author = {
     "name": mappings.StringField(name="name", index="analyzed", index_analyzer="standard", store=False).as_dict()
@@ -12,9 +15,14 @@ blogpost = {
             mappings.StringField(name="body", index="analyzed", index_anlayzer="standard", store=False),
             mappings.StringField(name="body_snowball", index="analyzed", index_analyzer="snowball", store=False),
             ]).as_dict(),
+    'summary': mappings.MultiField(name='summary', fields=[
+            mappings.StringField(name='summary', index='analyzed', index_analyzer='standard', store=False),
+            mappings.StringField(name='summary_snowball', index='analyzed', index_analyzer='snowball', store=False),
+            ]).as_dict(),
     "created_on": mappings.DateField(name="created_on", index="not_analyzed", store=False).as_dict(),
     "updated_on": mappings.DateField(name="updated_on", index="not_analyzed", store=False).as_dict(),
     "title": mappings.StringField(name="title", index="not_analyzed", store=False).as_dict(),
+    'slug': mappings.StringField(name='slug', index='not_analyzed', store=False).as_dict(),
     }
 
 comment = {
@@ -41,12 +49,12 @@ class Command(BaseCommand):
             {"post": {"_parent": {"type": "author"}, "properties": blogpost}},
             {"comment": {"properties": comment}},
             ]
-        es = pyes.es.ES()
+        es = pyes.es.ES(ELASTICSEARCH_URL)
         for _mapping_dict in mappings:
             _type = _mapping_dict.keys()[0]
             _mapping = {_type: _mapping_dict[_type]}
-            es.indices.create_index_if_missing("blog")
-            es.indices.put_mapping(doc_type=_type, indices="blog", mapping=_mapping)
+            es.indices.create_index_if_missing(ELASTICSEARCH_INDEX)
+            es.indices.put_mapping(doc_type=_type, indices=ELASTICSEARCH_INDEX, mapping=_mapping)
             self.stdout.write("-- %s\n" % _type)
             self.stdout.flush()
         self.stdout.write("Finished\n")
--- simple/views.py
+++ simple/views.py
@@ -1,21 +1,28 @@
 # Create your views here.
-from django.template import RequestContext
+from django.template import RequestContext, defaultfilters
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-from .models import get_post, get_posts, index_post, get_author, create_author, get_reference, index_comment
+from .models import get_post, get_post_by_slug, get_posts, index_post, get_author, create_author, get_reference, index_comment
 from .forms import BlogForm, CommentForm
 from pyes import ES
 from pyes.query import MatchAllQuery, FilteredQuery
 from pyes.filters import TermFilter, TermsFilter
 from datetime import datetime
 from django.conf import settings
+import markdown
 
 USE_AKISMET = getattr(settings, 'USE_AKISMET', True)
 
+
+def blog_detail_slug(request, slug):
+    post = get_post_by_slug(slug)
+    return render_to_response('simple/blogpost_detail.html',
+                              {'object': post},
+                              context_instance=RequestContext(request))
 
 def blog_detail(request, parent, pk):
     post = get_post(parent, pk)
@@ -24,7 +31,7 @@ def blog_detail(request, parent, pk):
                               context_instance=RequestContext(request))
 
 def blog_list(request):
-    posts = get_posts()
+    posts = get_posts('-created_on')
     return render_to_response('simple/blogpost_list.html',
                               {'object_list': posts},
                               context_instance=RequestContext(request))
@@ -43,16 +50,24 @@ def blog_edit(request, parent=None, pk=None):
     if request.method == 'POST':
         form = BlogForm(request.POST, initial=post)
         if form.is_valid():
+            body = form.cleaned_data['body']
+            if form.cleaned_data['is_markdown']:
+                body = markdown.markdown(body)
             post['author'] = author
             post['title'] = form.cleaned_data['title']
-            post['body'] = form.cleaned_data['body']
+            if form.cleaned_data['summary']:
+                post['summary'] = form.cleaned_data['summary']
+            post['body'] = body
+            post['slug'] = defaultfilters.slugify(post['title'])
             post['body_clean'] = form.cleaned_data['body']
             post['body_lower'] = form.cleaned_data['body']
             post['updated_on'] = datetime.utcnow()
             if 'created_on' not in post:
                 post['created_on'] = datetime.utcnow()
-            blog_id = index_post(author, post)
-            return HttpResponseRedirect(reverse('blogpost_detail', args=(author.get_meta().id, blog_id)))
+            blog_id = index_post(author, post, post['slug'])
+            return HttpResponseRedirect(reverse('blogpost_detail_by_slug', args=(blog_id,)))
+        else:
+            form = BlogForm(initial=post)
     else:
         form = BlogForm(initial=post)
     return render_to_response('simple/blogpost_form.html',
@@ -64,11 +79,19 @@ def blog_delete(request, parent, pk):
     pass
 
 def comment_edit(request, reference_type, reference_to):
+    referent = None
+    while reference_type != 'post':
+        # chase back to a blog post
+        referent = get_reference(reference_to, reference_type)
+        reference_to = referent.reference_to
+        reference_type = referent.reference_type
+    referent = get_reference(reference_to, reference_type)
     if request.method == 'POST':
         form = CommentForm(data=request.POST, reference_type=reference_type, reference_to=reference_to)
         if form.is_valid():
             comment = dict(comment=form.cleaned_data['comment'],
                            pingback=form.cleaned_data['pingback'],
+                           comment_author=form.cleaned_data['name'],
                            created_on=datetime.utcnow(),
                            reference_type=reference_type,
                            reference_to=reference_to)
@@ -96,13 +119,7 @@ def comment_edit(request, reference_type, reference_to):
                     comment['is_spam'] = True
             else:
                 comment['is_spam'] = False
-            referent = None
-            while reference_type != 'post':
-                # chase back to a blog post
-                referent = get_reference(reference_to, reference_type)
-                reference_to = referent.reference_to
-                reference_type = referent.reference_type
-                referent = get_reference(reference_to, reference_type)
+            comment['is_spam'] = False
             if referent:
                 comment['post_author'] = referent.author
                 comment['post'] = referent.get_meta().id
@@ -113,13 +130,6 @@ def comment_edit(request, reference_type, reference_to):
                 comment['post'] = referent.get_meta().id
                 comment['post_title'] = referent.title
             index_comment(comment)
-        return HttpResponseRedirect(reverse('blogpost_detail', args=(referent.author, referent.get_meta().id)))
-    else:
-        while reference_type != 'post':
-            # chase back to a blog post
-            referent = get_reference(reference_to, reference_type)
-            reference_to = referent.reference_to
-            reference_type = referent.reference_type
-            referent = get_reference(reference_to, reference_type)
-        return HttpResponseRedirect(reverse('blogpost_detail', args=(referent.author, referent.get_meta().id)))
+        return HttpResponseRedirect(reverse('blogpost_detail_by_slug', args=(referent.get_meta().id,)))
+    return HttpResponseRedirect(reverse('blogpost_detail_by_slug', args=(referent.get_meta().id,)))
 
