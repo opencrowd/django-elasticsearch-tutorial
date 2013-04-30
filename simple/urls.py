48ef5c91818078a256d218ccaa1b700b4c88da22
Steve Coursen <smcoursen@gmail.com>
2013-04-30 16:24:51 -0400

    Support viewing blog post by slug

--- simple/urls.py
+++ simple/urls.py
@@ -4,12 +4,13 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns(
-    'simple.views',
+    'blog.views',
     url(r'^comment/(?P<reference_type>.*?)/(?P<reference_to>.*?)/create/$', 'comment_edit', name='comment_create'),
+    url(r'^post/action/create/$', 'blog_edit', name='blogpost_create'),
     url(r'^post/(?P<parent>.*?)/(?P<pk>.*?)/edit/$', 'blog_edit', name='blogpost_edit'),
     url(r'^post/(?P<parent>.*?)/(?P<pk>.*?)/delete/$', 'blog_delete', name='blogpost_delete'),
     url(r'^post/(?P<parent>.*?)/(?P<pk>.*?)/$', 'blog_detail', name='blogpost_detail'),
-    url(r'^post/create/$', 'blog_edit', name='blogpost_create'),
+    url(r'^post/(?P<slug>.*?)/$', 'blog_detail_slug', name='blogpost_detail_by_slug'),
     url(r'^posts/$', 'blog_list', name='blogpost_list'),
     url(r'^$', 'blog_list'),
 )
